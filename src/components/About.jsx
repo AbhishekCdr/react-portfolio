92dfe800dc2b893aff3ca116a0e47386d2a1e178
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-03-05 10:44:35 +0530

    updating resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,22 +5,22 @@ const About = () => {
   return (
     <div className="grid w-full">
       <div
-        className="flex flex-col bg-cover bg-top m-3"
+        className="m-3 flex flex-col bg-cover bg-top"
         style={{ backgroundImage: "url('/static/images/about.png')" }}
       >
-        <div className="flex flex-col justify-self-auto items-center mt-10">
+        <div className="mt-10 flex flex-col items-center justify-self-auto">
           <div className="text-4xl font-bold">ABOUT ME</div>
         </div>
-        <div className="ml-5 mt-8 mr-5 flex flex-col items-center lg:flex-row lg:justify-between">
-          <div className="w-1/2 flex justify-center">
+        <div className="ml-5 mr-5 mt-8 flex flex-col items-center lg:flex-row lg:justify-between">
+          <div className="flex w-1/2 justify-center">
             <img
               src="/static/images/profile.png"
               alt="profile"
-              className="w-full lg:w-3/4 xl:w-1/2  mr-8 z-10 mb-5"
+              className="z-10 mb-5 mr-8  w-full lg:w-3/4 xl:w-1/2"
             />
           </div>
-          <div className="w-2/3 lg:h-auto flex justify-center z-10 ">
-            <p className="text-2xl text-center font-customFont1 font-bold mr-10">
+          <div className="z-10 flex w-2/3 justify-center lg:h-auto ">
+            <p className="mr-10 text-center font-customFont1 text-2xl font-bold">
               I am outgoing, dedicated, and open-minded. I work great with
               people and adjust to changes with ease. I believe that a person
               should work on developing their professional skills and learning
@@ -29,9 +29,9 @@ const About = () => {
           </div>
         </div>
 
-        <div className="py-10 flex justify-center">
+        <div className="flex justify-center py-10">
           <a
-            href="https://drive.google.com/file/d/1IkaCRnpiHWhejruOlCOCH9LPrSRrR4fk/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1jJLUCwSISCtomKawnQCjYY-niOO2lIiF/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -39,7 +39,7 @@ const About = () => {
               size="lg"
               variant="gradient"
               ripple={true}
-              className="hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 font-customFont font-thin"
+              className="font-customFont font-thin duration-300 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
             >
               Resume
             </Button>
